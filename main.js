4eff1a96da97e1224aa07df22424fe0ef6994fe2
Jarol Medina <145091749+Medina07P@users.noreply.github.com>
2025-03-22 22:52:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+
+var audio = document.getElementById("audio");
+var playButton = document.getElementById("playButton");
+
+playButton.addEventListener("click", function () {
+    if (audio.paused) {
+        audio.play().catch(error => console.log("Error al reproducir:", error));
+    } else {
+        audio.pause();
+    }
+});
